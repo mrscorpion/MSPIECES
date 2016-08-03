491b703b5713669b1e6b27103d6dacde961b55cf
mr.scorpion <mrscorpion1114@gmail.com>
2016-08-03 22:59:33 +0800

    add commit

---
+++ momentProgramme/momentProgramme/MVC/Basic/View/BSTableViewCell.m
@@ -0,0 +1,36 @@
+//
+//  BSTableViewCell.m
+//  momentProgramme
+//
+//  Created by mr.scorpion on 15/11/12.
+//  Copyright © 2015年 mr.scorpion. All rights reserved.
+//
+
+#import "BSTableViewCell.h"
+
+@implementation BSTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self setSelectionStyle:UITableViewCellSelectionStyleNone];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    [super initWithCoder:aDecoder];
+    if (self) {
+        [self setSelectionStyle:UITableViewCellSelectionStyleNone];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
